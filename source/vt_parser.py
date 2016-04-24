30a8174d63f7e5ab71f7b8833e00a400dd3badab
Saurabh Chakravarty <saurabc@yahoo.com>
2016-04-24 19:38:11 -0400

    Replaces the os call to run scripts.

--- source/vt_parser.py
+++ source/vt_parser.py
@@ -3,6 +3,7 @@ import json
 import sys
 import explicitconnective.evaluate_connective_classifier_maxent
 import subprocess
+import os
 
 class DiscourseParser(object):
 
@@ -24,7 +25,8 @@ class DiscourseParser(object):
     def run_explicit_args_extractor(self,input_parses_file ,output_path):
         input_relations_file = output_path + self.explicit_connective_output_file_name
         output_relations_file = output_path + self.explicit_args_output_file_name
-        return subprocess.call(["explicit_args/runExplicitArgs.sh", input_parses_file, input_relations_file, output_relations_file])
+        os.system("explicit_args/runExplicitArgs.sh %s %s %s %s" % (input_relations_file, input_parses_file, ".", output_relations_file))
+        #return subprocess.call(["explicit_args/runExplicitArgs.sh", input_parses_file, input_relations_file, ".", output_relations_file])
 
     def run_ps_arg1_extractor(self,input_parses_file, output_path):
         input_relations_file = output_path + self.explicit_args_output_file_name
@@ -44,13 +46,13 @@ if __name__ == '__main__':
     input_parses_file = sys.argv[1]
     output_dir = sys.argv[2]
     parser = DiscourseParser()
-    parser.run_connective_classifier(input_parses_file, output_dir)
+    #parser.run_connective_classifier(input_parses_file, output_dir)
     parser.run_explicit_args_extractor(input_parses_file, output_dir)
-    if parser.run_ps_arg1_extractor(input_parses_file, output_dir) == 0:
-        if parser.run_ps_arg2_extractor(input_parses_file, output_dir) == 0:
-            parser.run_explicit_sense_classifier(input_parses_file, output_dir)
-        else:
-            print "Error encountered while running the ps_arg2 extractor"
-    else:
-        print "Error encountered while running the ps_arg2 extractor"
+    # if parser.run_ps_arg1_extractor(input_parses_file, output_dir) == 0:
+    #     if parser.run_ps_arg2_extractor(input_parses_file, output_dir) == 0:
+    #         parser.run_explicit_sense_classifier(input_parses_file, output_dir)
+    #     else:
+    #         print "Error encountered while running the ps_arg2 extractor"
+    # else:
+    #     print "Error encountered while running the ps_arg2 extractor"
 
