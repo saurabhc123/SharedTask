5f786b1f24b42b082909305d8f54f2573b5bb3af
root <root@nlp1.cs.vt.edu>
2016-04-28 12:51:32 -0400

    Commenting out code that prints output.json

--- source/explicit_args/pc2.py
+++ source/explicit_args/pc2.py
@@ -234,7 +234,7 @@ def readInput(inputFilenamePath, inputParse, trainOrTest):
    #print "Sentence Number: " + str(parseJSON_sentence_number);
    #print "Connective Word ID: " + str(connectiveWordID);
    fileName = str(relation['DocID']);
-   relID = str(relation['ID']);
+
    #Parses.json object for that relation
    parseObject = en_parse_dict[relation_DocID]['sentences'][parseJSON_sentence_number];
    connectiveWordIDs = [];
@@ -391,13 +391,13 @@ def readInput(inputFilenamePath, inputParse, trainOrTest):
     arg2TokenList = [];
     arg1STokenList = [];
     arg2STokenList = [];
-    bigDiction[bigDictionIndex] = (features, label, relation_DocID, parseJSON_sentence_number, connectiveWordIDs, strConnectiveWords, arg1TokenList, arg2TokenList, arg1STokenList, arg2STokenList, relID);
+    bigDiction[bigDictionIndex] = (features, label, relation_DocID, parseJSON_sentence_number, connectiveWordIDs, strConnectiveWords, arg1TokenList, arg2TokenList, arg1STokenList, arg2STokenList);
     #featForConn[(relation_DocID, parseJSON_sentence_number, connectiveWordIDs)] = features;
     featForConnTs[(relation_DocID, parseJSON_sentence_number, tuple(connectiveWordIDs))] = features;
     bigDictionIndex = bigDictionIndex + 1;
      
    if(trainOrTest == 'train'):
-    obigDiction[bigDictionIndex] = (features, label, relation_DocID, parseJSON_sentence_number, connectiveWordIDs, strConnectiveWords, arg1TokenList, arg2TokenList, arg1STokenList, arg2STokenList, relID);
+    obigDiction[bigDictionIndex] = (features, label, relation_DocID, parseJSON_sentence_number, connectiveWordIDs, strConnectiveWords, arg1TokenList, arg2TokenList, arg1STokenList, arg2STokenList);
     featForConnTr[(relation_DocID, parseJSON_sentence_number, tuple(connectiveWordIDs))] = features;
     bigDictionIndex = bigDictionIndex + 1;
     trainingSet.append((features, label));
@@ -1128,15 +1128,14 @@ def mergeSS(constituents, predictedArray, conns_list, parse_dict):
  for i, dictEntry in enumerate(conns_list):
   DocID = dictEntry[2];
   sent_index = dictEntry[3];
-  conn_indices = dictEntry[4];
-  relID = dictEntry[10]; 
+  conn_indices = dictEntry[4]; 
   #DocID, sent_index, conn_indices = conn
   if relation_dict.has_key(i):
    Arg1_list, Arg2_list = relation_dict[i]
    Arg1_list = merge_NT_Arg(Arg1_list, parse_dict, DocID, sent_index)
    Arg2_list = merge_NT_Arg(Arg2_list, parse_dict, DocID, sent_index)
    if Arg1_list != [] and Arg2_list != []:
-    temp.append((source, DocID, sent_index, conn_indices, Arg1_list, Arg2_list, relID))
+    temp.append((source, DocID, sent_index, conn_indices, Arg1_list, Arg2_list))
    else:
     #print "Filename For Zero: " + str(DocID);
     #print "Sent Index For Zero: " + str(sent_index);
@@ -1185,7 +1184,6 @@ def printToFile(relFile, outFile, temp, parse_dict, ps_array):
    sent_index = data[2];
    entryDict = str(docID);
    dictEntry['DocID'] = str(docID);
-   dictEntry['ID'] = str(data[6]);
    arg1 = get_doc_offset(parse_dict, docID, data[2],data[4]);
    arg1Final = [];
    elementI = 0
@@ -1241,16 +1239,16 @@ def printToFile(relFile, outFile, temp, parse_dict, ps_array):
    #dictEntry['Arg1Pos'] = 'SS'; 
    json.dump(dictEntry, f)
    f.write("\n");
-  '''
+
  print "Dict Sentence: " + str(dictSentenceToken);
  print "Dict Document: " + str(dictDocumentToken);
+ '''
  with open(relFile, 'a') as p:
   for ps_entry in ps_array:
    dictEntry = dict();
    filename = ps_entry[2];
    #dictEntry['DocID'] = str(ps_entry[2]);
    sent_index = ps_entry[3];
-   relID = ps_entry[10];
    arg1DocIndex = [];
    arg2DocIndex = [];
    arg1SentIndex = [];
@@ -1275,11 +1273,16 @@ def printToFile(relFile, outFile, temp, parse_dict, ps_array):
    for e in ps_entry[4]:
     conn_indices.append(e+1);
    conn_indices_final = get_doc_offset(parse_dict, filename, sent_index - 1, conn_indices);
+   connFinal = [];
+   elementI = 0
+   for connI in conn_indices_final:
+    connFinal.append([0, 0, connI, sent_index, conn_indices[elementI]]);
+    elementI = elementI + 1;
    dictEntry['DocID'] = str(filename);
-   dictEntry['ID'] = str(relID);
    dictEntry['Arg1'] = dict({"TokenList":arg1Final});
    dictEntry['Arg2'] = dict({"TokenList":arg2Final});
-   dictEntry['Connective'] = dict({"TokenList":conn_indices});
+   dictEntry['Connective'] = dict({"TokenList":connFinal});
+   #dictEntry['Connective'] = dict({"TokenList":conn_indices});
    dictEntry['Sense'] = ["Expansion.Conjunction"];
    dictEntry['Type'] = 'Explicit';
    dictEntry['Arg1Pos'] = 'PS';
